cefce5133a5783ddd6e9563fbbfd252a9fcbbc98
ali sait bilgi <alisaitbilgi@gmail.com>
2017-02-09 16:11:07 +0300

    sparkline is added, last version is ready (#5)
    * sparkline is added, last version is ready
    
    * gitignore is updated
    
    * React-Redux, React-Router and Iframe Applied version
    
    * last version is ready
    
    * DS_Store is added to gitignore
    
    * update! generic xhr api is published on npm and dowloaded as dev-dependcy
    
    * Media Queries are applyied
    
    * styles are updated, responsive design tried to be applied
    
    * small style changes done

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,24 @@
 import React, {Component} from "react";
-import FormContainer from "./FormContainer";
+import HomeContainer from "./HomeContainer";
 import forkImage from "../../public/styles/images/fork.png";
 
 
-class App extends Component {
+export default class App extends Component {
 
   render() {
     return (
         <div className="main">
-            <article>
-                <FormContainer/>
-            </article>
-            <nav>
-                <a href={"https://github.com/alisaitbilgi"}>
-                    <img className="forkImage" alt="fork-me" src={forkImage} />
-                </a>
-            </nav>
-            <aside />
+            <div className="sidebar">
+              <a href={"https://github.com/alisaitbilgi/GithubBadge-React-Redux"}>
+                <img className="fork-image" alt="fork-me" src={forkImage} />
+              </a>
+            </div>
+            <div className="article">
+                <HomeContainer/>
+            </div>
+            <div className="sidebar"></div>
         </div>
     );
   }
 }
 
-export default App;
